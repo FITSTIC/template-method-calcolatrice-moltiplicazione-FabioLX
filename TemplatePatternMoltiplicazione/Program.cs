c314a7e2d1e9fdf2b55ff3d6572fcefb035f6ca0
rarodroid@gmail.com <rarodroid@gmail.com>
2017-10-30 12:14:53 +0100

    refactor

--- TemplatePatternMoltiplicazione/Program.cs
+++ TemplatePatternMoltiplicazione/Program.cs
@@ -12,20 +12,17 @@ namespace TemplatePatternMoltiplicazione
       
         static void Main(string[] args)
         {
-            //ULTIMO STEP in classe main creo gli oggetti
+           
             var calcModern = new CalcolatriceModerna();
             calcModern.FaiOperazione(2, 2);
 
             var calcAntica = new CalcolatriceAntica();
             calcAntica.FaiOperazione(2, 3);
-
-
-
+            
         }
     }
-
-
-    // creare una classe PADRE di comportamento, che i figli dovranno definire e implementare
+    
+   
     abstract class Calcolatrice: ICalcolatrice
     {
         protected int valore1;
@@ -47,16 +44,13 @@ namespace TemplatePatternMoltiplicazione
             Console.WriteLine ("risultato moltiplicazione: "+ result);
         }
 
-    
-
-        //creo metodo pubblico visibile al figlio
+            
         public void FaiOperazione(int valore1, int valore2)
         {
 
             Valorizza(valore1, valore2);
             Moltiplica();
-            Result();
-                       
+            Result();                      
         }
 
     }
@@ -65,10 +59,10 @@ namespace TemplatePatternMoltiplicazione
         void FaiOperazione(int valore1, int valore2);
     }
 
-
-    //creo documento FIGLIO A
+    
     class CalcolatriceModerna : Calcolatrice
     {
+        //calcolo moltiplicazione diretta
         protected override void Moltiplica()
         {
             this.result = this.valore1 * this.valore2;
@@ -80,7 +74,8 @@ namespace TemplatePatternMoltiplicazione
     {
         protected override void Moltiplica()
         {
-           
+
+            //calcolo moltiplicazione con ciclo e somma
             for (int i = 0; i < this.valore2; i++)
             {
                 this.result += this.valore1;
